9711042fb279eff82107859666d68972e33a8c68
Jonathan C Dosmann <jon_dosmann@dwahcg.com>
2018-10-17 08:27:41 -0400

    Initial Project Commit

---
+++ CroweHelloWorld/CroweHelloWorld.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CroweHelloWorld.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
